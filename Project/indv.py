0eb93bc24fd1817c665c81737a80d644ad8d0a6b
IngaBesedina <inga.besedina@mail.ru>
2024-05-18 14:10:06 +0300

    modified

--- Project/indv.py
+++ Project/indv.py
@@ -3,6 +3,7 @@
 
 import json
 import sys
+
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
@@ -11,16 +12,12 @@ def get_student():
     """
     Запросить данные о студенте.
     """
-    surname = input("Введите фамилию и инициалы: ")
+    surname = input("Фамилия и инициалы: ")
     group_num = input("Введите номер группы: ")
-    print('Введите оценки: ')
+    print("Введите оценки: ")
     grades = [int(n) for n in input().split()]
 
-    student = {
-        'name': surname,
-        'group_number': group_num,
-        'grades': grades
-    }
+    student = {"name": surname, "group_number": group_num, "grades": grades}
 
     return student
 
@@ -32,19 +29,13 @@ def display_students(staff):
     # Проверить, что список студентов не пуст.
     if staff:
         # Заголовок таблицы.
-        line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-            '-' * 4,
-            '-' * 30,
-            '-' * 20,
-            '-' * 14
+        line = "+-{}-+-{}-+-{}-+-{}-+".format(
+            "-" * 4, "-" * 30, "-" * 20, "-" * 14
         )
         print(line)
         print(
-            '| {:^4} | {:^30} | {:^20} | {:^14} |'.format(
-                "№",
-                "Ф.И.О.",
-                "Группа",
-                "Оценки"
+            "| {:^4} | {:^30} | {:^20} | {:^14} |".format(
+                "№", "Ф.И.О.", "Группа", "Оценки"
             )
         )
         print(line)
@@ -52,11 +43,11 @@ def display_students(staff):
         # Вывести данные о всех студентах.
         for idx, student in enumerate(staff, 1):
             print(
-                '| {:>4} | {:<30} | {:<20} | {:>14} |'.format(
+                "| {:>4} | {:<30} | {:<20} | {:>14} |".format(
                     idx,
-                    student.get('name', ''),
-                    student.get('group_number', ''),
-                    ', '.join(str(el) for el in student.get('grades'))
+                    student.get("name", ""),
+                    student.get("group_number", ""),
+                    ", ".join(str(el) for el in student.get("grades")),
                 )
             )
         print(line)
@@ -69,7 +60,7 @@ def select_students(staff):
     # Сформировать список студентов, имеющих оценки 4 и 5.
     result = []
     for student in staff:
-        if all(grade >= 4 for grade in student['grades']):
+        if all(grade >= 4 for grade in student["grades"]):
             result.append(student)
 
     # Возвратить список выбранных студентов.
@@ -118,23 +109,23 @@ def main():
         command = input(">>> ").lower()
 
         # Выполнить действие в соответствие с командой.
-        if command == 'exit':
+        if command == "exit":
             break
 
-        elif command == 'add':
+        elif command == "add":
             # Запросить данные о работнике.
             student = get_student()
 
             # Добавить словарь в список.
             students.append(student)
             # Сортировка по возрастанию среднего балла
-            students.sort(key=lambda x: sum(x['grades']) / 5)
+            students.sort(key=lambda x: sum(x["grades"]) / 5)
 
-        elif command == 'list':
+        elif command == "list":
             # Отобразить всех студентов.
             display_students(students)
 
-        elif command == 'select':
+        elif command == "select":
             selected = select_students(students)
             # Отобразить выбранных студентов.
             display_students(selected)
@@ -157,7 +148,7 @@ def main():
             # Сохранить данные в файл с заданным именем.
             students = load_students(file_name)
 
-        elif command == 'help':
+        elif command == "help":
             # Вывести справку о работе с программой.
             print("Список команд:\n")
             print("add - добавить студента;")
@@ -172,5 +163,5 @@ def main():
             print(f"Неизвестная команда {command}", file=sys.stderr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
